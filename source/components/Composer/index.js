3bf11ddb44f70ef624b90bea68556ff70a1f2e76
alinak <AlinaK@Radware.com>
2019-06-12 10:08:18 +0300

    functionality of adding post

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react';
 import { Consumer } from 'components/HOC/withProfile';
+import { PropTypes } from 'prop-types';
 
 //Instruments
 import Styles from './styles.m.css';
 
 export class Composer extends Component {
+    constructor() {
+        super();
+        this._updateComment = this._updateComment.bind(this);
+        this._submitComment = this._submitComment.bind(this);
+    }
+
+    state = {
+        comment: '',
+    }
+
+    _updateComment(event) {
+        this.setState({
+            comment: event.target.value,
+        });
+    }
+
+    _submitComment(event) {
+        event.preventDefault();
+        const { comment } = this.state;
+
+        if (!comment) {
+            return null;
+        }
+
+        this.props._createPost(comment);
+        this.setState({
+            comment: '',
+        });
+    }
+
     render() {
+        const { comment } = this.state;
+
         return (
             <Consumer>
                 {
                     (context) => (
                         <section className = { Styles.composer }>
                             <img src = { context.avatar } />
-                            <form>
-                                <textarea placeholder = { `What's on your mind ${context.currentUserFirstName}?` } />
+                            <form onSubmit = { this._submitComment }>
+                                <textarea
+                                    placeholder = { `What's on your mind ${context.currentUserFirstName}?` }
+                                    value = { comment }
+                                    onChange = { this._updateComment }
+                                />
                                 <input
                                     type = 'submit'
                                     value = 'Post'
@@ -26,3 +63,6 @@ export class Composer extends Component {
         );
     }
 }
+Composer.propTypes = {
+    _createPost: PropTypes.func.isRequired,
+};
--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
 
 import * as Components from 'components';
-
+import { getUniqueID } from '../../instruments/index';
+import moment from 'moment';
 import Styles from './styles.m.css';
 
 export class Feed extends Component {
+    constructor() {
+        super();
+        this._createPost = this._createPost.bind(this);
+    }
+
+    state = {
+        posts: [
+            {id: 'wedwefdwsc', created: 1560310675, comment: 'Hey there!'},
+            {},
+        ],
+    }
+
+    _createPost(comment) {
+        const post = {
+            id:      getUniqueID(),
+            created: moment.utc(),
+            comment,
+        };
+        this.setState(({ posts }) => ({
+            posts: [ post, ...posts ],
+        }));
+    }
+
     render() {
+        const { posts } = this.state;
+
+        const postsJSX = posts.map((post) => {
+            return (
+                <Components.Post
+                    key = { post.id }
+                    { ...post }
+                />
+            );
+        });
+
         return (
             <section className = { Styles.feed }>
-                <Components.StatusBar/>
-                <Components.Composer/>
-                <Components.Post/>
+                <Components.StatusBar />
+                <Components.Composer _createPost = { this._createPost } />
+                {postsJSX}
             </section>
         );
     }
